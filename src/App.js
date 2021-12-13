a25f4689c2913afa56037ea4dc098f2cbc0932b4
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-12-14 00:42:45 +0100

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import axios from 'axios'
 import './App.css';
 import Header from './components/ui/Header';
 import CharacterGrid from './components/characters/CharacterGrid';
+import Search from './components/ui/Search';
 
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       setItems(result.data)
       setIsLoading(false)
     }
     fetchItems()
 
-  }, [])
+  }, [query])
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
