b158a60f7fba29cb7edcbcfc198ed02840d31f9c
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-12-14 00:00:42 +0100

    Added styling and handled data fetching

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CharacterGrid = ({ items, isLoading }) => {
+    return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
+        {items.map(item => (
+            <h1 key={item.char_id}>{item.name}</h1>
+        ))}
+    </section>)
+}
+
+export default CharacterGrid
