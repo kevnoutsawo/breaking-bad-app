3554745c38cd398034137ff39157fe38a69fbec8
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-12-14 00:53:57 +0100

    Added Loading spinner

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
 
 const CharacterGrid = ({ items, isLoading }) => {
-    return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
+    return isLoading ? (<Spinner />) : (<section className="cards">
         {items.map(item => (
             <CharacterItem key={item.char_id} item={item}></CharacterItem>
         ))}
