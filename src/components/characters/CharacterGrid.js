f0657ec46f1aaa840c4c923d34519661e9e0cf16
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2021-12-14 00:18:19 +0100

    Created Character Item and Grid components

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import CharacterItem from './CharacterItem'
 
 const CharacterGrid = ({ items, isLoading }) => {
     return isLoading ? (<h1>Loading...</h1>) : (<section className="cards">
         {items.map(item => (
-            <h1 key={item.char_id}>{item.name}</h1>
+            <CharacterItem key={item.char_id} item={item}></CharacterItem>
         ))}
     </section>)
 }
